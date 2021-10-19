7460fda3d63c13b3f45c5713ad2a8e92359a866a
Andrew Hinh <ajhinh@gmail.com>
2021-10-18 22:32:03 -0700

    .gitignore fix

--- modules/vis/.ipynb_checkpoints/loss-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import torch 
-import torch.nn as nn
-def MultiLabelSmoothingLoss(pred, target, epsilon=0.05, reduction=None):
-    temp = target.detach().clone()
-    temp.requires_grad = True
-    for i, x in enumerate(temp): temp[i] = torch.where(x==1.0, 1.0-epsilon, epsilon)
-    smoothed_criterion = nn.BCEWithLogitsLoss()(pred, temp)
-    return smoothed_criterion
